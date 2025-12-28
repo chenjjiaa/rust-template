20fc69ab0431fa492bdba25378538707c33b3986
Cheems <94773058+itscheems@users.noreply.github.com>
2025-12-29 00:01:53 +0800

    chore: update config

--- crates/cmd/src/main.rs
+++ crates/cmd/src/main.rs
@@ -1,4 +1,4 @@
-// Copyright 2025 chenjjiaa
+// Copyright 2025 itscheems
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
